de8a7c29ebda7cb88117542156aae545ef622885
EddyRamon1 <31424601+EddyRamon1@users.noreply.github.com>
2018-10-25 13:28:59 -0400

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,24 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10) # It did something
+
+end1 = "B"
+end2 = "o"
+end3 = "n"
+end4 = "e"
+end5 = "l"
+end6 = "e"
+end7 = "s"
+end8 = "s"
+end9 = "C"
+end10 = "h"
+end11 = "i"
+end12 = "c"
+end13 = "k"
+end14 = "e"
+end15 = "n"
+
+# I don't like where this is going
+print(end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8, end=' ')
+print(end9 + end10 + end11 + end12 + end13 + end14 + end15)
