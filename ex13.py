33e98a87740e736728e1a79b57ac44562077d6ad
EddyRamon1 <31424601+EddyRamon1@users.noreply.github.com>
2018-10-25 13:30:57 -0400

    Create ex13.py

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+# Something about reading the WYSS section
+script, first, second, third= argv
+
+argv = input("Insert variables: ")
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
