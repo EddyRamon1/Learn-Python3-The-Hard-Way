12dbf0c190162841bf79d963f5bed9da0d5ef1a4
EddyRamon1 <31424601+EddyRamon1@users.noreply.github.com>
2018-10-25 13:28:16 -0400

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,18 @@
+name = 'Eddy J. Ramon'
+age = 16 # not a lie
+height = 63 # inches
+weight = 125 # lbs
+eyes = 'Brown'
+teeth = 'Yellowish-whitish'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("It's not that heavy for his height and age.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the food.")
+
+# I will try to get the next line right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight}, I get {total}.")
