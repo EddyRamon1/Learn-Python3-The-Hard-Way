bfd414d8e38268e74f76f45809434b61c2e0b35e
EddyRamon1 <31424601+EddyRamon1@users.noreply.github.com>
2018-10-25 13:29:36 -0400

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# I have to type it correctly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Hear are the days: ", days)
+print("Hear are the months: ", months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
