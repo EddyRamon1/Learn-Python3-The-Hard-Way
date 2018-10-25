1a1f1fed72c8a9ea2517353e7f49fd8acc2a7158
EddyRamon1 <31424601+EddyRamon1@users.noreply.github.com>
2018-10-25 13:36:06 -0400

    Create ex27.py

---
+++ ex27.py
@@ -0,0 +1,39 @@
+#NOT = TRUE?
+not (False)
+not (True)
+
+#OR = TRUE?
+True or False
+True or True
+False or True
+False or False
+
+#AND = TRUE?
+True and False
+True and True
+False and True
+False and False
+
+#NOT OR TRUE?
+not(True or False)
+not(True or True)
+not(False or True)
+not(False or False)
+
+#NOT AND = TRUE?
+not(True and False)
+not(True and True)
+not(False and True)
+not(False and False)
+
+#"!=" = TRUE?
+1 != 0
+1 != 1
+0 != 1
+0 != 0
+
+#"==" = TRUE?
+1 == 0
+1 == 1
+0 == 1
+0 == 0
