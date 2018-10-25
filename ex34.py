5d6326c6ee9f79d24b52a5d105e0d7bd61414afa
EddyRamon1 <31424601+EddyRamon1@users.noreply.github.com>
2018-10-25 13:38:26 -0400

    Create ex34.py

---
+++ ex34.py
@@ -0,0 +1,19 @@
+animals = ['bear', 'python3.6', 'peacock','kangaroo', 'whale', 'platypus']
+bear = animals[0]
+
+1. The animal at 1
+print(f"Bear")
+2. The third (3rd) animal.
+print(f"The animal at 0 is the 3rd animal and is a peacock")
+3. The first (1st) animal.
+print(f"The animal at 0 is the 1st animal and is a bear")
+4. The animal at 3.
+print(f"peacock")
+5. The fifth (5th) animal.
+print(f"The animal at 0 is the 5th animal and is a whale")
+6. The animal at 2.
+print(f"python3.6")
+7. The sixth (6th) animal.
+print(f"The animal at 0 is the 6th animal and is a platypus")
+8. The animal at 4.
+print(f"kangaroo")
