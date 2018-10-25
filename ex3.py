45f37a349639b84dc51e391ffca5b41fcaf3cb7d
EddyRamon1 <31424601+EddyRamon1@users.noreply.github.com>
2018-10-25 13:27:32 -0400

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# This line prints text text in the terminal
+print("I will now count my chickens:")
+
+# It prints out and calculates the text with the total number printed out
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+# then it prints out another line of text
+print("Now I will count the eggs:")
+
+# It prints out the result of this calculation 
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# prints out a text line that asks is this equation true
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# then it prints out not the output of the equation, but it states if the code above is true. In this case, it's false
+print(3 + 2 < 5 - 7)
+
+# It prints out the quoted statementment and calculates the equation after the comma in no quotation marks.
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# It prints out the response to the false statement.
+print("Oh, that's why it's False.")
+
+# it prints out a question of more examples.
+print("How about some more.")
+
+# It prints out true or false whenever each statement is either less/greater than or equal to,
+print("is it greater?", 5 > -2)
+print("is it greater or equal?", 5 >= -2)
+print("is it less or equal?", 5 <= -2)
